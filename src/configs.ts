d59f7dfdacd48cf6e325afa01ba81ecc7c86a6cc
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-01-21 17:21:51 +0530

    setup: prisma schema

--- src/configs.ts
+++ src/configs.ts
@@ -8,4 +8,6 @@ export default {
 		port: (process.env.SERVER_PORT && +process.env.SERVER_PORT) || 3000,
 		name: process.env.SERVER_NAME || "server_name",
 	},
+	databaseUrl:
+		process.env.DATABASE_URL ?? "postgresql://postgres@localhost:5432/postgres",
 }
