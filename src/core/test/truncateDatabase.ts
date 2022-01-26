835518d923774e79cac3f70ffe93b9bff15a2407
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-01-26 18:14:43 +0530

    fix: truncate database

--- src/core/test/truncateDatabase.ts
+++ src/core/test/truncateDatabase.ts
@@ -3,4 +3,13 @@ import { PrismaClient } from "@prisma/client"
 export async function truncateDatabase() {
 	const prisma = new PrismaClient()
 	await prisma.admin.deleteMany()
+	await prisma.googleOAuth.deleteMany()
+	await prisma.offering.deleteMany()
+	await prisma.lawyerBank.deleteMany()
+	await prisma.lawyer.deleteMany()
+	await prisma.user.deleteMany()
+	await prisma.city.deleteMany()
+	await prisma.state.deleteMany()
+	await prisma.language.deleteMany()
+	await prisma.paperType.deleteMany()
 }
