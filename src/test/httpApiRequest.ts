c2d148b1223477136ef6d0c1dff2b10579fe7d04
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-01-30 13:39:56 +0530

    test: setup multipart request with httpApiRequest

--- src/test/httpApiRequest.ts
+++ src/test/httpApiRequest.ts
@@ -1,4 +1,5 @@
 import { expect } from "chai"
+import _ from "lodash"
 import { HttpMethod, HttpStatusCode } from "../core/http"
 import { requester } from "./setup"
 
@@ -6,32 +7,63 @@ export async function httpApiRequest({
 	method,
 	endpoint,
 	body,
+	files,
 	auth,
 	expectedStatusCode = HttpStatusCode.OK,
+	isMultipartFormData,
 }: {
 	method: HttpMethod
 	endpoint: string
 	body?: string | object
+	files?: {
+		[key: string]:
+			| Parameters<typeof client.attach>[1]
+			| Parameters<typeof client.attach>[1][]
+	}
 	auth?: string
 	expectedStatusCode?: HttpStatusCode
+	isMultipartFormData?: boolean
 }): Promise<any> {
 	const client = method === "post" ? requester.post(endpoint) : requester.get(endpoint)
 
 	const authToken = auth || undefined
 	if (authToken) client.set("Authorization", `Bearer ${authToken}`)
-	if (body) client.send(body)
+	if (isMultipartFormData) {
+		client.set("Content-Type", "multipart/form-data")
 
-	const response = await executeHttpRequest(client)
+		if (body) {
+			for (let [key, value] of Object.entries(body)) {
+				if (typeof value !== "string") {
+					if (!["number" || "boolean"].includes(typeof value)) {
+						throw new Error(
+							`Invalid value type for form-data request: ${typeof value}`,
+						)
+					}
+					value = value.toString()
+				}
+				client.field(key, value)
+			}
+		}
+
+		if (files) {
+			let i = 0
+			for (let [key, value] of Object.entries(files)) {
+				if (_.isArray(value)) {
+					value.forEach((file) => client.attach(key, file, `file-${i++}.jpg`))
+				} else {
+					client.attach(key, value, `file-${i++}.jpg`)
+				}
+			}
+		}
+	} else {
+		if (body) client.send(body)
+	}
+
+	const response = await client
 	expect(response.status).equal(
 		expectedStatusCode,
 		`${response.body?.name}: ${response.body?.message}  ${response.body?.stack}`,
 	)
 
 	return response.body
-}
-
-async function executeHttpRequest(
-	client: ReturnType<typeof requester.post>,
-): Promise<ChaiHttp.Response> {
-	return await client
 }
