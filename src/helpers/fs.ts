52ea1c35d6713964aac504705432ed40052e1dd2
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-01-31 13:06:36 +0530

    fix: copyFile dest dir issue

--- src/helpers/fs.ts
+++ src/helpers/fs.ts
@@ -11,9 +11,8 @@ export async function copyFile({
 	dest: PathLike
 	fileName: string
 }) {
-	const destDir = path.dirname(dest.toString())
-	await createDirIfNotExists(destDir)
-	await fs.copyFile(src, path.join(destDir, fileName))
+	await createDirIfNotExists(dest)
+	await fs.copyFile(src, path.join(dest.toString(), fileName))
 }
 
 export async function createDirIfNotExists(dir: PathLike) {
