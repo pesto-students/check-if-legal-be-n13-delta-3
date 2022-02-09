ea68f2bd6a5f2a97466006d815bf7c2d80ac0f1b
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-02-09 16:30:45 +0530

    fix: remove pagination from state list

--- src/services/state/listState.ts
+++ src/services/state/listState.ts
@@ -4,14 +4,8 @@ import { prisma } from "../../core/prisma"
 
 export async function listState({
 	filter: { id } = {},
-	pagination: { limit = 10, pageNo = 1 } = {},
 }: {
 	filter?: { id?: number }
-	pagination?: { limit?: number; pageNo?: number }
 } = {}): Promise<State[]> {
-	return await prisma.state.findMany({
-		where: { ...(id && { id }) },
-		...(limit && { take: limit }),
-		...(pageNo && { skip: (pageNo - 1) * limit }),
-	})
+	return await prisma.state.findMany({ where: { ...(id && { id }) } })
 }
