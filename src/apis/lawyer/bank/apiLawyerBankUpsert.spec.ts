6da098f5c6522dee2d30ca8d5dc95fa997f0ddf2
Kunal Gosrani <iamkunalgosrani@gmail.com>
2022-02-22 14:29:51 +0530

    test: lawyer bank upsert api

--- src/apis/lawyer/bank/apiLawyerBankUpsert.spec.ts
+++ src/apis/lawyer/bank/apiLawyerBankUpsert.spec.ts
@@ -1,48 +1,71 @@
-// import { randAccount, randBrand } from "@ngneat/falso"
-// import { Lawyer } from "@prisma/client"
-// import { expect } from "chai"
-// import { AuthRole } from "../../../core/enums"
-// import { HttpMethod, HttpStatusCode } from "../../../core/http"
-// import { createAuthToken } from "../../../helpers/auth/authToken"
-// import { httpApiRequest } from "../../../test/httpApiRequest"
-// import { randomBankIfsc } from "../../../test/resources/bank"
-// import { generateLawyer } from "../../../test/resources/lawyer"
-// import { truncateDatabase } from "../../../test/truncateDatabase"
+import { randAccount, randBrand } from "@ngneat/falso"
+import { Lawyer } from "@prisma/client"
+import { expect } from "chai"
+import { AuthRole } from "../../../core/enums"
+import { HttpMethod, HttpStatusCode } from "../../../core/http"
+import { createAuthToken } from "../../../helpers/auth/authToken"
+import { getLawyerBank } from "../../../services/lawyerBank/getLawyerBank"
+import { httpApiRequest } from "../../../test/httpApiRequest"
+import { randomBankIfsc } from "../../../test/resources/bank"
+import { generateLawyer } from "../../../test/resources/lawyer"
+import { truncateDatabase } from "../../../test/truncateDatabase"
 
-// const method = HttpMethod.POST
-// const endpoint = "/lawyer/bank"
+const method = HttpMethod.PUT
+const endpoint = "/lawyer/bank"
 
-// describe(`API: ${method} ${endpoint}`, () => {
-// 	let lawyer: Lawyer
-// 	let auth: string
+describe(`API: ${method} ${endpoint}`, () => {
+	let lawyer: Lawyer
+	let auth: string
 
-// 	before(async () => {
-// 		await truncateDatabase()
-// 		lawyer = await generateLawyer({ isVerified: true })
-// 		auth = createAuthToken({ id: lawyer.userId, role: AuthRole.LAWYER })
-// 	})
+	before(async () => {
+		await truncateDatabase()
+		lawyer = await generateLawyer({ isVerified: true })
+		auth = createAuthToken({ id: lawyer.userId, role: AuthRole.LAWYER })
+	})
 
-// 	it(`Success`, async () => {
-// 		const bankName = randBrand()
-// 		const bankIfsc = randomBankIfsc()
-// 		const accountNumber = randAccount()
+	it(`Success: create new bank`, async () => {
+		const bankName = randBrand()
+		const bankIfsc = randomBankIfsc()
+		const accountNumber = randAccount()
 
-// 		const res = await httpApiRequest({
-// 			method,
-// 			endpoint,
-// 			auth,
-// 			body: { bankName, bankIfsc, accountNumber },
-// 			expectedStatusCode: HttpStatusCode.CREATED,
-// 		})
-// 		expect(res).exist
-// 		expect(res.id).exist
+		await httpApiRequest({
+			method,
+			endpoint,
+			auth,
+			body: { bankName, bankIfsc, accountNumber },
+			expectedStatusCode: HttpStatusCode.NO_CONTENT,
+		})
 
-// 		const [lawyerBank] = await listLawyerBank({ filter: { id: res.id } })
-// 		expect(lawyerBank).exist
-// 		expect(lawyerBank.id).equal(res.id)
-// 		expect(lawyerBank.lawyerId).equal(lawyer.id)
-// 		expect(lawyerBank.bankName).equal(bankName)
-// 		expect(lawyerBank.bankIfsc).equal(bankIfsc)
-// 		expect(lawyerBank.accountNumber).equal(accountNumber)
-// 	})
-// })
+		const lawyerBank = await getLawyerBank({ lawyerId: lawyer.id })
+		expect(lawyerBank).exist
+		if (lawyerBank) {
+			expect(lawyerBank.lawyerId).equal(lawyer.id)
+			expect(lawyerBank.bankName).equal(bankName)
+			expect(lawyerBank.bankIfsc).equal(bankIfsc)
+			expect(lawyerBank.accountNumber).equal(accountNumber)
+		}
+	})
+
+	it(`Success: update bank`, async () => {
+		const bankName = randBrand()
+		const bankIfsc = randomBankIfsc()
+		const accountNumber = randAccount()
+
+		await httpApiRequest({
+			method,
+			endpoint,
+			auth,
+			body: { bankName, bankIfsc, accountNumber },
+			expectedStatusCode: HttpStatusCode.NO_CONTENT,
+		})
+
+		const lawyerBank = await getLawyerBank({ lawyerId: lawyer.id })
+		expect(lawyerBank).exist
+		if (lawyerBank) {
+			expect(lawyerBank.lawyerId).equal(lawyer.id)
+			expect(lawyerBank.bankName).equal(bankName)
+			expect(lawyerBank.bankIfsc).equal(bankIfsc)
+			expect(lawyerBank.accountNumber).equal(accountNumber)
+		}
+	})
+})
